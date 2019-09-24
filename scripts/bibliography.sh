56daed646ba7deb2d6dd9a443f1d1dff56c730d6
Hendrik Timmermann <hendrik.timmermann@msg.group>
2019-09-24 11:11:57 +0200

    IFE-93: Erweiterung der Skripte auf IF-Erweiterungen und Verbesserung des Bibliographie-Skriptes auf Erkennung von 2 Einträgen in einer Zeile

--- scripts/bibliography.sh
+++ scripts/bibliography.sh
@@ -3,20 +3,49 @@
 # Erzeugt für Dokumente individuelle Literaturverzeichnisse
 #
 # Funktionsweise:
-# - common/bibliography.adoc enthält alle Literaturrefenzen in der Form [[[BibRef]]]. Baue eine Liste aller Referenzen (1)
+# - common/bibliography.adoc enthält alle Literaturrefenzen in der Form [[[BibRef]]].
+#   Baue eine Liste aller Referenzen (1)
 # - Suche in adoc-Dateien aller Dokumente (2) nach Referenzen der Form <<BibRef>> (3)
-# - Extrahiere für jede gefundene Referenz den Eintrag aus bibliography.adoc und übernehme in individuelle
-#   bibliography.adoc des Dokuments (4)
+# - Extrahiere für jede gefundene Referenz den Eintrag aus bibliography.adoc und
+#   übernehme in individuelle bibliography.adoc des Dokuments (4)
 
 # (1)
-allBibRefs=($(gawk 'match($0, /\[\[\[(.+)\]\]\]/, m) { print m[1]}' common/bibliography.adoc))
+allBibRefs() {
+    gawk 'match($0, /\[\[\[(.+)\]\]\]/, m) { print m[1]}' $curDir/common/bibliography.adoc | sort -u
+}
 
 # (3)
 findRefs() {
-    for ref in $@
+
+
+    if [ -f $dir/grabbedRefs.txt ];
+    then
+      rm $dir/grabbedRefs.txt
+    fi
+    touch $dir/grabbedRefs.txt
+    cat $dir/docinfo.adoc $dir/thisdoc.adoc $dir/inhalt.adoc $dir/anhaenge.adoc > $dir/RefSrc-temp.adoc
+
+    for Ref2 in $@
     do
-        cat $dir/docinfo.adoc $dir/thisdoc.adoc $dir/inhalt.adoc $dir/anhaenge.adoc $dir/glossary.adoc | gawk -v foundref=$ref 'match($0, /<<([a-zA-Z0-9]+)>>/, m) && m[1] == foundref { print m[1]; }' RS=" " | sort -u
+      cat $dir/RefSrc-temp.adoc | gawk '{while(match($0,/<<([^<>]+)>>/)) {print substr($0,RSTART+2,RLENGTH-4); $0=substr($0,RSTART+RLENGTH)}}' | grep $Ref2 | sort -u >> $dir/grabbedRefs.txt
     done
+
+    touch $dir/grabbedRefs-temp.txt
+    cat $dir/grabbedRefs.txt | sort -u > $dir/grabbedRefs-temp.txt
+    cat $dir/grabbedRefs-temp.txt > $dir/grabbedRefs.txt
+
+    cat $dir/grabbedRefs.txt
+
+    if [ -f $dir/grabbedRefs-temp.txt ];
+    then
+      rm $dir/grabbedRefs-temp.txt
+    fi
+
+    if [ -f $dir/grabbedRefs.txt ];
+    then
+      rm $dir/grabbedRefs.txt
+    fi
+    rm $dir/RefSrc-temp.adoc
 }
 
 # (4)
@@ -30,18 +59,48 @@ buildDocumentBibliography() {
 
         for ref in $@
         do
-          gawk -v foundref=$ref 'match($1, /\[\[\[(.+)\]\]\]/, m) && m[1] == foundref { print $0 }' RS='' FS='\n' common/bibliography.adoc | tee -a $dir/bibliography.adoc
+          gawk -v foundref=$ref 'match($1, /\[\[\[(.+)\]\]\]/, m) && m[1] == foundref { print $0 }' RS='' FS='\n' $curDir/common/bibliography.adoc | tee -a $dir/bibliography.adoc
         done
     else
         touch $dir/bibliography.adoc
     fi
 }
 
-allDocDirectories=($(find $1 -name master.adoc | xargs dirname))
+allDocDirCmd() {
+    find $ArgOneDir/10_* $ArgOneDir/20_* -name master.adoc | xargs dirname
+}
+
+# (1)
+echo "Generating document dependent bibliographies...."
+
+curDir=$(pwd)
+#echo "DEBUG: started in " $curDir
+
+# wechsele in das übergebene Verzeichnis
+cd $1
+ArgOneDir=$(pwd)
+
+#echo "DEBUG: got ArgOne " $ArgOneDir
+
+allDocDirectories=($(eval "allDocDirCmd"))
+
+# wechsele in das übergebene Arbeitsverzeichnis
+cd $curDir
 
 # (2)
+
+allBibEntries=($(eval "allBibRefs"))
+
+for igt in ${allBibEntries[@]}; do
+  echo " BibEntries : " $igt
+done
+
 for dir in ${allDocDirectories[@]}
 do
-    bibRefs=$(findRefs ${allBibRefs[@]})
-    buildDocumentBibliography ${bibRefs[@]}
+  echo " Ziel: " $dir
+  bibRefs=($(findRefs "${allBibEntries[@]}"))
+  #for igt22 in ${bibRefs[@]}; do
+  # echo " BibRef : " $igt22
+  #done
+  buildDocumentBibliography "${bibRefs[@]}"
 done
